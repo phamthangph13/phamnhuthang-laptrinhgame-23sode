2d8552d1946b6ad7c09986e42383587d3ec7c559
phamthangph13 <winnieph13@gmail.com>
2026-01-15 10:24:24 +0700

    nop bai tap

---
+++ Assets/logging.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class logging : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Nguyễn Đình Tuấn Anh");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Nguyễn Đình Tuấn Anh");
+    }
+}
---
+++ Assets/logscript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class logscript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("PHAM NHU THANG - NOP BAI");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("PHAM NHU THANG - NOP BAI");
+    }
+}
